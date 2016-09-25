57c274b29e9dc00a2b88efed0bf4414ad30326d1
Emiliakazoo <emiliakazoo@gmail.com>
2016-09-25 08:59:11 -0700

    First commit for the refactor

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 
 $(document).ready(function(){
-	var x = 100;
-	for (var i = 1; i <= x; i++) {
+
+var usrNbr;
+$("#fizz-buzz-entry-form").on("submit",function(event){
+		event.preventDefault();     
+    	var usrInput = $("#fizz-buzz-entry").val();
+
+		if(isNaN(usrInput)) {
+			alert("Please enter a REAL number, dawg.");
+			$("#fizz-buzz-entry").val("").focus();
+			return;
+		} else if (usrInput % 1 != 0){
+			alert("A WHOLE number, please.  No decimals.");
+			$("#fizz-buzz-entry").val("").focus();
+		} else{
+			usrNbr = +usrInput;    
+	    	$("#fizz-buzz-entry").val("");	
+	    	fizzBuzz(usrNbr);
+		}
+
+
+});
+
+function fizzBuzz(usrNbr){
+
+	for (var i = 1; i <= usrNbr; i++) {
 		if((i % 3 == 0) && (i % 5 == 0)){
 			$("#fizzBuzzUL").append("<li>Fizz Buzz");
 		}
@@ -18,6 +41,8 @@ $(document).ready(function(){
 			//console.log(i);
 		}
 	}
+
+}
 
 });
 
